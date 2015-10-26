c30123cf322ad38e70888a40a9f6246fff6c67ea
khoa.ln <khoa.ln@gzone.com.vn>
2015-10-26 21:58:07 +0700

    Initial Commit

---
+++ iShare/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iShare
+//
+//  Created by Khoa Le on 10/26/15.
+//  Copyright © 2015 Khoa Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
